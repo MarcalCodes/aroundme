f94d3e805497f709dcd8baf4eb3a8f6a5b12f92c
Ingrid Marcal <marcal.delaine@gmail.com>
2025-04-15 20:20:03 +1000

    implement "get all events of a user" API (#10)

--- examples.sql
+++ examples.sql
@@ -1,12 +1,17 @@
 -- Get all the subscriptions for a given user ID
 SELECT *
 FROM Subscription
-WHERE userId = ${id};
+WHERE userId = ${userId};
 
--- Get all the Areas a users is subscribed to
+-- Get all the Areas a user is subscribed to
 SELECT a.*
 FROM Subscription as s, Area as a
 WHERE
-    s.userId = ${id}
+    s.userId = ${userId}
     AND s.areaId = a.id -- joining Subscription and Area tables on Subscription::areaId and Area::id fields
 ;
+
+-- Get all the Events a user created
+SELECT *
+FROM Event
+WHERE creatorId = ${userId}
